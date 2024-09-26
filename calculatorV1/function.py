cf46cf60b1462e66bb014906337e3bd1bc45a28c
wtc <wtc@pop-os.localdomain>
2024-09-26 10:45:59 +0200

    First Calc version

---
+++ calculatorV1/function.py
@@ -0,0 +1,13 @@
+#This function carries the functinality of a basic calculator app
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def multiply(n1, n2):
+    return n1 * n2
